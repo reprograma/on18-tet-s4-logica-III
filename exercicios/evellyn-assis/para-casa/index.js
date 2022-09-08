93f2ebebb0a351216d16a2307ed136a1d4066443
evyassis <109322210+evyassis@users.noreply.github.com>
2022-09-08 17:36:45 -0300

    adicionar exercicio resolvido de array e objetos

--- exercicios/evellyn-assis/para-casa/index.js
+++ exercicios/evellyn-assis/para-casa/index.js
@@ -2,65 +2,86 @@ let filmes = [
   {
     titulo: "us",
     genero: "terror",
-    ano: "2019", 
+    ano: "2019",
   },
   {
     titulo: "transformers: O Lado Oculto da Lua",
     genero: ["ação", "sci-fi"],
-    ano: "2011", 
+    ano: "2011",
   },
   {
     titulo: "moonfall - ameaça lunar",
-    genero: ['Aventura', "sci-fi"],
-    ano: "2022", 
+    genero: ['aventura', "sci-fi"],
+    ano: "2022",
   },
   {
     titulo: "casa gucci",
     genero: "drama",
-    ano: "2021", 
+    ano: "2021",
   },
   {
     titulo: "encanto",
     genero: ["animação", "musical"],
-    ano: "2021", 
+    ano: "2021",
   },
   {
     titulo: "Demon Slayer - Mugen Train",
     genero: ["ação", "fantasia"],
-    ano: "2019", 
+    ano: "2019",
   },
   {
     titulo: "O Último Suspiro",
     genero: ["drama", "sci-fi"],
-    ano: "2019", 
+    ano: "2019",
   },
   {
     titulo: "Suspiria",
     genero: "terror",
-    ano: "1977", 
+    ano: "1977",
   },
   {
     titulo: "Corpo Elétrico",
     genero: ["drama", "ficção"],
-    ano: "2017", 
+    ano: "2017",
   },
   {
     titulo: "Amour",
     genero: ["drama", "romance"],
-    ano: "2012", 
+    ano: "2012",
   },
   {
     titulo: "Ninho de Musaranho",
     genero: "terror",
-    ano: "2014", 
+    ano: "2014",
   },
   {
     titulo: "Colonia",
     genero: "suspense",
-    ano: "2015", 
+    ano: "2015",
   },
   {
     titulo: "Homem-Aranha: Sem Volta para Casa",
     genero: ["ação", "aventura"],
-    ano: "2021", 
-  }] 
+    ano: "2021",
+  }]
+
+
+function buscarFilmes(filme) {
+  let filmeEncontrado = filmes.filter(function(elemento) {
+    return elemento.titulo.toLowerCase() == filme.toLowerCase()
+    || elemento.titulo.toLowerCase().includes(filme.toLowerCase())
+  })
+
+  return filmeEncontrado
+}
+
+function buscarGenero(generoPesquisado) {
+  let generoEncontrado = filmes.filter(function(elemento) {
+    return elemento.genero.includes(generoPesquisado.toLowerCase())
+  })
+
+  return generoEncontrado
+}
+
+console.log(buscarFilmes("Homem"))
+console.log(buscarGenero("terror"));
